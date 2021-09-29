f9873a025aadf159c18a4d7a00ce55408fe66993
Vinicius Montes <viniciusmontes@outlook.com.br>
2021-09-29 15:59:14 -0300

    User entity and resource

---
+++ src/main/java/com/educandoweb/course/resources/UserResource.java
@@ -0,0 +1,20 @@
+package com.educandoweb.course.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.educandoweb.course.entites.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+
+	@GetMapping
+	public ResponseEntity<User> findAll() {
+		User u = new User(1L, "Vinicius", "viniciusmontes@outlook.com.br", "1198142-0699", "112131");
+		return ResponseEntity.ok().body(u);
+	}
+
+}

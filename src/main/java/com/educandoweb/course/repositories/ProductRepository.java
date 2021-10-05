fc1e0bb16d880be4e0839869e266e56863d7956d
Vinicius Montes <viniciusmontes@outlook.com.br>
2021-10-05 18:19:00 -0300

    Product

---
+++ src/main/java/com/educandoweb/course/repositories/ProductRepository.java
@@ -0,0 +1,10 @@
+package com.educandoweb.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.educandoweb.course.entites.Product;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+	
+
+}
